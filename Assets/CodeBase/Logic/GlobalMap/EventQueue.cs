5a1bc4883d7ed7f0150ea477bd8d448c117223bb
JanisZ1 <Jan4uks753@mail.ru>
2023-11-27 12:46:36 +0200

    Added circle event code to EventQueue class

--- Assets/CodeBase/Logic/GlobalMap/EventQueue.cs
+++ Assets/CodeBase/Logic/GlobalMap/EventQueue.cs
@@ -7,11 +7,27 @@ namespace Assets.CodeBase.Logic.GlobalMap
     public class EventQueue : MonoBehaviour
     {
         public event Action<Vector2> SiteEventHappened;
-
         [SerializeField] private List<Vector2> _sites = new List<Vector2>();
+
+        public event Action<float> CircleEventHappened;
+        private List<float> _circleEvents;
 
         private void Update() =>
             CheckSiteEvents();
+
+        public void AddToCircleEventList(float directrixYOnEventMoment) =>
+            _circleEvents.Add(directrixYOnEventMoment);
+
+        public void CheckCircleEvents()
+        {
+            for (int i = 0; i < _circleEvents.Count; i++)
+            {
+                if (ScanningLine.Directrix.y <= _circleEvents[i])
+                {
+                    InvokeCircleEventWithMaxY(i);
+                }
+            }
+        }
 
         private void CheckSiteEvents()
         {
@@ -24,6 +40,12 @@ namespace Assets.CodeBase.Logic.GlobalMap
         {
             SiteEventHappened?.Invoke(_sites[i]);
             _sites.Remove(_sites[i]);
+        }
+
+        private void InvokeCircleEventWithMaxY(int i)
+        {
+            CircleEventHappened?.Invoke(_circleEvents[i]);
+            _circleEvents.Remove(_circleEvents[i]);
         }
     }
 }
