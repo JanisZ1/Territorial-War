7b3aa8f5dd21f401bf29c6b62c3a9567fba72f80
JanisZ1 <Jan4uks753@mail.ru>
2023-12-03 17:37:34 +0200

    Vertex class

---
+++ Assets/CodeBase/Logic/GlobalMap/Vertex.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.CodeBase.Logic.GlobalMap
+{
+    public class Vertex
+    {
+        public readonly Vector2 Position;
+
+        public Vertex(Vector2 position) =>
+            Position = position;
+    }
+}
